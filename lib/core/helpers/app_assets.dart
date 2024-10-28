8e668e7331e4044dd2d32b6067cc309d7891ed7f
A7med_Nage7 <a.nageh@bayanatz.com>
2024-10-28 22:04:07 +0300

    splash screen done

--- lib/core/helpers/app_assets.dart
+++ lib/core/helpers/app_assets.dart
@@ -1,4 +1,6 @@
 class AppAssets {
   static const String geminiLottie = 'assets/gemini_lottie.json';
   static const String emptyHistoryLottie = 'assets/empty_chat_history.json';
+  static const String geminiLogoDark = 'assets/gemini_logo_dark.png';
+  static const String geminiLogoLight = 'assets/gemini_logo_light.png';
 }
